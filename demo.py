82911e3924f67d8e72aaab93e778d662c7d22cdf
cxhy <979009228@qq.com>
2016-12-25 23:53:49 +0800

    initial the project

---
+++ demo.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import re
+
+page = 1
+url = 'http://www.qiushibaike.com/hot/page/' + str(page)
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)'#Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6)
+headers = { 'User-Agent' : user_agent }
+try:
+    request = urllib2.Request(url,headers = headers)
+    response = urllib2.urlopen(request)
+    content = response.read().decode('utf-8')
+    pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class'+
+                         '="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
+    items = re.findall(pattern,content)
+    for item in items:
+        haveImg = re.search("img",item[3])
+        if not haveImg:
+            print item[0],item[1],item[2],item[4]
+except urllib2.URLError, e:
+    if hasattr(e,"code"):
+        print e.code
+    if hasattr(e,"reason"):
+        print e.reason          
