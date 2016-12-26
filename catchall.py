e732f5edb30a3b31273d97a83b47e00b1ed0f2f8
cxhy <979009228@qq.com>
2016-12-27 00:00:41 +0800

    success catch word

--- catchall.py
+++ catchall.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 import urllib
 import urllib2
-import re
 
 page = 1
 url = 'http://www.qiushibaike.com/text/page/' + str(page)
@@ -10,11 +9,7 @@ headers = { 'User-Agent' : user_agent }
 try:
     request = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(request)
-    content = response.read().decode('utf-8')
-    pattern = re.compile('<div.*?class="author.*?>.*?<h2>(.*?)</h2>.*?class="content">.*?<span>(.*?)</span>',re.S)
-    items = re.findall(pattern,content)
-    for item in items:
-        print item[0],item[1]
+    print response.read()
 except urllib2.URLError, e:
     if hasattr(e,"code"):
         print e.code
